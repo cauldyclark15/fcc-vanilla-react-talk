39c25097b33d3b4cf8f6d8eb8020d1dce18c046d
cauldyclark15 <jcing.jc@gmail.com>
2017-11-19 13:21:00 +0800

    Add login route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 5000;
 
 server.use(express.static(path.join(__dirname, 'dist')));
 
+server.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist', 'login.html'));
+});
+
 server.listen(port, function() {
 	console.log('yes its working');
 });
