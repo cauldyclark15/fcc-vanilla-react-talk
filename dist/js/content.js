d03eec5fc7e0a7063d1c046cc5d4ce3621aca292
cauldyclark15 <jcing.jc@gmail.com>
2017-11-19 15:16:53 +0800

    Make add form work

--- dist/js/content.js
+++ dist/js/content.js
@@ -4,6 +4,35 @@
 		.then(items => {
 			return createRowsInBody(items);
 		});
+
+	const addItemForm = document.getElementById('addItemForm');
+
+	addItemForm.addEventListener('submit', event => {
+		event.preventDefault();
+		const { name, email, phone } = event.target;
+
+		const usersBody = document.getElementById('userTableBody');
+
+		const nameText = document.createTextNode(name.value);
+		const emailText = document.createTextNode(email.value);
+		const phoneText = document.createTextNode(phone.value);
+
+		const tr = document.createElement('tr');
+
+		const tdName = document.createElement('td');
+		const tdEmail = document.createElement('td');
+		const tdPhone = document.createElement('td');
+
+		tdName.appendChild(nameText);
+		tdEmail.appendChild(emailText);
+		tdPhone.appendChild(phoneText);
+
+		tr.appendChild(tdName);
+		tr.appendChild(tdEmail);
+		tr.appendChild(tdPhone);
+
+		usersBody.appendChild(tr);
+	});
 })();
 
 const createRowsInBody = function(items = []) {
